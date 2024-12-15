13c8c20896fb31899db3ee86cf5da7c7cbc00927
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-15 15:36:13 +0700

    menambahkan method updateMahasiswa

--- app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.pertemuan10.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -23,5 +24,11 @@ interface MahasiswaDao {
     //deleteMahasiswa
     @Delete
     suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    //updateMahasiswa
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
+
 
 }
