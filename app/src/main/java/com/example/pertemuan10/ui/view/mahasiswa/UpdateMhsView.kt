1b96929fc37ff12f7d6daae881eca94e251d1cac
irajesicajanuaris <jjiraaa1@gmail.com>
2024-12-16 19:29:21 +0700

    menambahkan file UpdateMhsView

---
+++ app/src/main/java/com/example/pertemuan10/ui/view/mahasiswa/UpdateMhsView.kt
@@ -0,0 +1,92 @@
+package com.example.pertemuan10.ui.view.mahasiswa
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Update
+import com.example.monitoringapplication.ui.costumwidget.TopAppBar
+import com.example.pertemuan10.ui.viewmodel.PenyediaViewModel
+import com.example.pertemuan10.ui.viewmodel.UpdateMhsViewModel
+import com.example.praktikumpertemuan10.ui.view.mahasiswa.InsertBodyMhs
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Composable
+fun UpdateMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory) //inisialisasi ViewModel
+){
+    val uiState = viewModel.updateUIState // Ambil UI state dari ViewModel
+    val snackbarHostState = remember { SnackbarHostState() }// snackstate
+    val coroutineScope = rememberCoroutineScope()
+
+    //observasi perubahan snackbarMessage
+    LaunchedEffect(uiState.snackbarMessage) {
+        println("LaunchedEffect triggered")
+        uiState.snackbarMessage?.let { message ->
+            println("Snackbar message received: $message")
+            coroutineScope.launch {
+                print("Launching coroutine for snackbar")
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }//tempatkan snackbar di scaffold
+        topBar = {
+            TopAppBar(
+                judul = "Edit Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+            )
+        }
+    ){ padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ){
+
+            // idi body
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)// update state di viewmodel
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()){
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main){
+                                onNavigate()// navigasi di main Thread
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
